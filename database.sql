707664da084ec4bd4600615534b21a40335426e5
Christian Dominic Ledesma <53726725+ChrisLedesma@users.noreply.github.com>
2020-01-23 11:40:04 +0800

    Update database.sql

--- database.sql
+++ database.sql
@@ -1,18 +1,17 @@
-CREATE TABLE `tbl_stocks` (
-  `sto_id` int(6) unsigned NOT NULL auto_increment, 
-  `sto_name` varchar(80) NOT NULL default '',
-  `sto_description` varchar(200) NOT NULL default '',
-  `sto_price` decimal(10,2) NOT NULL default '0.00',
-  PRIMARY KEY  (`sto_id`)
-) ENGINE=MyISAM AUTO_INCREMENT=100001;
+CREATE DATABASE db_pets;
 
-INSERT INTO tbl_stocks (sto_name, sto_description, sto_price)
-VALUES ('Shoes', 'Sports Shoes', '2500');
-INSERT INTO tbl_stocks (sto_name, sto_description, sto_price)
-VALUES ('Dress', 'Formal Dress', '1000');
-INSERT INTO tbl_stocks (sto_name, sto_description, sto_price)
-VALUES ('Hat', 'Hat Men', '500');
-INSERT INTO tbl_stocks (sto_name, sto_description, sto_price)
-VALUES ('Hat', 'Hat Female', '500');
-INSERT INTO tbl_stocks (sto_name, sto_description, sto_price)
-VALUES ('Bag', 'Sports Bag', '1500');
+USE db_pets;
+
+CREATE TABLE `tbl_pet` (
+	`pet_id` INT(5) UNSIGNED NOT NULL AUTO_INCREMENT,
+	`pet_name` VARCHAR(180) NOT NULL DEFAULT '',
+  `pet_type` VARCHAR(180) NOT NULL DEFAULT '',
+	`pet_age` INT(2) NOT NULL DEFAULT '0',
+	PRIMARY KEY (`pet_id`)
+)
+COLLATE='latin1_swedish_ci'
+ENGINE=MyISAM
+AUTO_INCREMENT=10000
+;
+INSERT INTO tbl_pets (pet_name, pet_type, pet_age)
+VALUES ('Mochi', 'Dog', '1');
